3e54663ea4f250aed70700ead807e0591c834ed7
Emma Selvig <emmaselvignielsen@gmail.com>
2021-05-13 20:03:59 +0200

    created index

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,2 +1,9 @@
 class HikesController < ApplicationController
+
+  def index
+    if params[:query].present?
+      @hikes = Hike.search_by_title_and_description(params[:query])
+    else
+      @hikes = Hike.all
+  end
 end
