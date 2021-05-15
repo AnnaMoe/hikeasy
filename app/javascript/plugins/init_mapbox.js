e47bd1a4c19548ed0b28eafbadcb8f49e01d310b
Anna Moelling <anna.moelling@googlemail.com>
2021-05-15 16:48:11 +0200

    added geocoding gem, mapbox, implemented world map on the homepage

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,4 +1,4 @@
-import mapboxgl from 'mapbox-gl';
+import mapboxgl, { accessToken } from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const initMapbox = () => {
@@ -6,6 +6,7 @@ const initMapbox = () => {
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    console.log(mapboxgl.accessToken)
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
@@ -18,6 +19,17 @@ const initMapbox = () => {
         .setPopup(popup)
         .addTo(map);
     });
+
+    const fitMapToMarkers = (map, markers) => {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+      map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+    };
+
+    if (mapElement) {
+      // [ ... ]
+      fitMapToMarkers(map, markers);
+    }
   }
 
 };
--- app/models/hike.rb
+++ app/models/hike.rb
@@ -3,8 +3,8 @@ class Hike < ApplicationRecord
 
   ## vitor suggestion, easy fix. create an address model and db table
   ## hike will have foreign keys (has one start address, has one destination address), this way each instance of address has only one thing to geocode.
-  geocoded_by :start_address, latitude: :start_latitude, longitude: :start_longitude
+  # geocoded_by :start_address, latitude: :start_latitude, longitude: :start_longitude
   geocoded_by :destination_address, latitude: :destination_latitude, longitude: :destination_longitude
-  after_validation :geocode, if: :will_save_change_to_start_address?
+  # after_validation :geocode, if: :will_save_change_to_start_address?
   after_validation :geocode, if: :will_save_change_to_destination_address?
 end
