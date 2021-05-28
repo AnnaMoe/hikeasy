d266f969d7a2b188cf285a738003ab7cf619fca0
Vitor Reis <vitor.reis7@gmail.com>
2021-05-29 00:00:08 +0200

    fix pundit / authentication error

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -4,15 +4,15 @@ class HikesController < ApplicationController
 
 
   def index
-      @hikes = policy_scope(Hike).order(created_at: :desc)
-      @hikes = @hikes.filter_by_difficulty(params[:difficulty]) if params[:difficulty]
-      @hikes = @hikes.filter_by_accomodation(params[:accomodation]) if params[:accomodation]
+    @hikes = policy_scope(Hike).order(created_at: :desc)
+    @hikes = @hikes.filter_by_difficulty(params[:difficulty]) if params[:difficulty]
+    @hikes = @hikes.filter_by_accomodation(params[:accomodation]) if params[:accomodation]
 
-      if params[:price]
-        min = params[:price].first.empty? ? 400 : params[:price].first.to_i
-        max = params[:price].last.empty? ? 800 : params[:price].last.to_i
-        @hikes = @hikes.filter_by_price(min,max)
-      end
+    if params[:price]
+      min = params[:price].first.empty? ? 400 : params[:price].first.to_i
+      max = params[:price].last.empty? ? 800 : params[:price].last.to_i
+      @hikes = @hikes.filter_by_price(min,max)
+    end
 
     #@hikes = policy_scope(Hike).order(created_at: :desc)
     @markers = @hikes.map do |hike|
@@ -41,6 +41,7 @@ class HikesController < ApplicationController
   end
 
   def toggle_favorite
+    authorize @hike
     current_user.favorited?(@hike) ?
     current_user.unfavorite(@hike) : 
     current_user.favorite(@hike)
