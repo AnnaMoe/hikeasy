d01868c5560e92e9049f4d13fdbb28f5112a2d32
Emma Selvig <emmaselvignielsen@gmail.com>
2021-05-29 15:43:02 +0200

    added buttons and changed layout of filtering options

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -6,13 +6,25 @@ class HikesController < ApplicationController
   def index
     @hikes = policy_scope(Hike).order(created_at: :desc)
     @hikes = @hikes.filter_by_difficulty(params[:difficulty]) if params[:difficulty]
-    @hikes = @hikes.filter_by_accomodation(params[:accomodation]) if params[:accomodation]
 
     if params[:price]
       min = params[:price].first.empty? ? 400 : params[:price].first.to_i
       max = params[:price].last.empty? ? 800 : params[:price].last.to_i
-      @hikes = @hikes.filter_by_price(min,max)
+      @hikes = @hikes.filter_by_price(min, max)
     end
+    if params[:distance]
+      min = params[:distance].first.empty? ? 24 : params[:distance].first.to_i
+      max = params[:distance].last.empty? ? 60 : params[:distance].last.to_i
+      @hikes = @hikes.where distance: min..max
+    end
+    if params[:length]
+      min = params[:length].first.empty? ? 2 : params[:length].first.to_i
+      max = params[:length].last.empty? ? 9 : params[:length].last.to_i
+      @hikes = @hikes.filter_by_length(min, max)
+    end
+
+
+
 
     #@hikes = policy_scope(Hike).order(created_at: :desc)
     @markers = @hikes.map do |hike|
@@ -43,7 +55,7 @@ class HikesController < ApplicationController
   def toggle_favorite
     authorize @hike
     current_user.favorited?(@hike) ?
-    current_user.unfavorite(@hike) : 
+    current_user.unfavorite(@hike) :
     current_user.favorite(@hike)
   end
 
--- app/models/hike.rb
+++ app/models/hike.rb
@@ -9,7 +9,8 @@ class Hike < ApplicationRecord
   scope :filter_by_region, ->(region_id) { where region_id: region_id }
   scope :filter_by_accomodation, ->(accomodation) { where accomodation_type: accomodation }
   scope :filter_by_price, ->(min, max) { where price: min..max }
-  scope :filter_by_lenght, ->(min, max) { where length: min..max }
+  scope :filter_by_length, ->(min, max) { where length: min..max }
+  scope :filter_by_distance, ->(min, max) { where distance: min..max }
 
   belongs_to :chatroom
 
