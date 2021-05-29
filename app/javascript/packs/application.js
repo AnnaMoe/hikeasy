d27470563b68bf4e1603e579f04f61a49ff9a3a0
Anna Moelling <anna.moelling@googlemail.com>
2021-05-29 17:41:06 +0200

    fetch weather api

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,6 +27,7 @@ import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
 import { initMapboxShow } from '../plugins/init_mapbox';
 import { initChatroomCable } from '../channels/chatroom_channel';
+import { initWeather } from '../components/init_weather';
 
 
 // Internal imports, e.g:
@@ -36,12 +37,7 @@ document.addEventListener('turbolinks:load', () => {
   initMapbox();
   initMapboxShow();
   initChatroomCable();
+  initWeather();
+  
 });
 
-const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=50.9616923&lon=13.9386643&appid=33723607922dc4022ad15bf945b658cb'
-
-fetch(url)
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data)
-  });
