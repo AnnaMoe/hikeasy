61d2658ed3e416e9e7e705169884659d302b9833
Anna Moelling <anna.moelling@googlemail.com>
2021-05-29 15:10:33 +0200

    add api and fetch

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -37,3 +37,11 @@ document.addEventListener('turbolinks:load', () => {
   initMapboxShow();
   initChatroomCable();
 });
+
+const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=50.9616923&lon=13.9386643&appid=33723607922dc4022ad15bf945b658cb'
+
+fetch(url)
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data)
+  });
