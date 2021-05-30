f89f6d7b76f95ac4d89e5d5f5f4c7eb039eedd4f
Anna Moelling <anna.moelling@googlemail.com>
2021-05-30 21:13:30 +0200

    added credit card columns, added validation in booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :hike
   has_many :reviews
+
+  validates :credit_card_expiration_month, :credit_card_expiration_year, :credit_card_cvc, presence: true
 end
