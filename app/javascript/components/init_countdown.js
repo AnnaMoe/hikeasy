560d0d45fd70737de4b4ec644cc08c9db0d68c08
Anna Moelling <anna.moelling@googlemail.com>
2021-06-04 21:23:49 +0200

    Implement countdown

--- app/javascript/components/init_countdown.js
+++ app/javascript/components/init_countdown.js
@@ -1,26 +1,32 @@
 // Set the date we're counting down to
-
-
 // Update the count down every 1 second
 const initCountdown = () => {
-  const countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
-  // Get today's date and time
-  const now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  const distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-
+  const demoElement = document.getElementById("booking-show-countdown");
+  
   // If the count down is finished, write some text
+  if (demoElement) {
+    const refDate = demoElement.dataset.referenceDate;
+    const countDownDate = new Date(refDate).getTime();
+    // Get today's date and time
+    const now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    const html = `<li><span class="days">${days}</span><p class="days_text">Days</p></li>
+              <li class="seperator">:</li>
+              <li><span class="hours">${("00" + hours.toString()).slice(-2)}</span><p class="hours_text">Hours</p></li>
+              <li class="seperator">:</li>
+              <li><span class="minutes">${minutes}</span><p class="minutes_text">Minutes</p></li>`;
+
+    // Display the result in the element with id="demo"
+    demoElement.innerHTML = html;
+  }
 };
 
 export { initCountdown };
