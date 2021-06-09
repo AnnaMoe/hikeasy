7bb29fec9f8d6ccd54cf3f9d8f3f166da0ba42dc
Anna Moelling <anna.moelling@googlemail.com>
2021-06-09 13:38:51 +0200

    meta tags after checklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :age, :country])
   end
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   include Pundit
 
   # Pundit: white-list approach.
