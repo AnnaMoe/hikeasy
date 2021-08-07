2b932d98afbbb2937b84746eaac438d176bba370
Anna Moelling <anna.moelling@googlemail.com>
2021-08-07 18:07:07 +0200

    add amount to booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,5 +4,7 @@ class Booking < ApplicationRecord
   belongs_to :group_hike
   has_many :reviews
 
+  monetize :amount_cents
+
   validates :credit_card, :credit_card_expiration_month, :credit_card_expiration_year, :credit_card_cvc, presence: true
 end
